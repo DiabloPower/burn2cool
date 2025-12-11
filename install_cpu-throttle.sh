2bf185a55f8b0c63a15f3dd6a57066cb70890d21
DiabloPower <diablopower@users.noreply.github.com>
2025-12-11 23:24:39 +0100

    Force enable systemd service for --yes flag to ensure automatic service installation and activation

--- install_cpu-throttle.sh
+++ install_cpu-throttle.sh
@@ -509,8 +509,8 @@ component_selection() {
   
   local selected=()
   if [ "${YES:-0}" -eq 1 ] || [ "${AUTO_YES:-0}" -eq 1 ]; then
-    # For non-interactive: use defaults, but web API always enabled for --yes
-    selected=("${defaults[0]}" "${defaults[1]}" "${defaults[2]}" "${defaults[3]}" "${defaults[4]}" "true" "${defaults[6]}" "${defaults[7]}")
+    # For non-interactive: use defaults, but web API and service always enabled for --yes
+    selected=("${defaults[0]}" "${defaults[1]}" "${defaults[2]}" "${defaults[3]}" "${defaults[4]}" "true" "${defaults[6]}" "true")
     if [ "${ENABLE_FIREWALL:-0}" -eq 1 ]; then
       selected[6]="true"
     fi
